3cdba67240b3d1ad2b8cc0525aeb1b55469078ea
JHR1986 <jhroche1986@gmail.com>
2021-03-04 12:49:10 +0000

    newstuff

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,44 +1,38 @@
 var map;
             var InforObj = [];
             var centerCords = {
-                lat: 34.9934,
-                lng: 134.9694
+
+                lat: 38.2682,
+                lng: 140.8694
             };
             var markersOnMap = [{
-                    placeName: "Japan (Tokyo)",
+                    placeName: "Tokyo",
                     LatLng: [{
                         lat: 35.6762,
                         lng: 139.6503
                     }]
                 },
                 {
-                    placeName: "Japan (Kyoto)",
+                    placeName: "Kyoto",
                     LatLng: [{
                         lat: 35.0116,
                         lng: 135.7681
                     }]
                 },
                 {
-                    placeName: "Japan (Hiroshima)",
+                    placeName: "Hiroshima",
                     LatLng: [{
                         lat: 34.3853,
                         lng: 132.4553
                     }]
                 },
                 {
-                    placeName: "Australia (Gold Coast)",
-                    LatLng: [{
-                        lat: -28.013044,
-                        lng: 513.425586
+                    placeName: "Sapporo",
+                    LatLng: [{ 
+                        lat: 43.0618,
+                        lng: 141.3545
                     }]
                 },
-                {
-                    placeName: "Australia (Perth)",
-                    LatLng: [{
-                        lat: -31.951994,
-                        lng: 475.858081
-                    }]
-                }
             ];
 
             window.onload = function () {
@@ -47,8 +41,8 @@ var map;
 
             function addMarkerInfo() {
                 for (var i = 0; i < markersOnMap.length; i++) {
-                    var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
-                        '</h1><p>Lorem ipsum dolor sit amet, vix mutat posse suscipit id, vel ea tantas omittam detraxit.</p></div>';
+                    var contentString = '<div id="content"><h2>' + markersOnMap[i].placeName +
+                        '</h2><p>Lorem ipsum dolor sit amet, vix mutat posse suscipit id, vel ea tantas omittam detraxit.</p></div>';
 
                     const marker = new google.maps.Marker({
                         position: markersOnMap[i].LatLng[0],
@@ -88,7 +82,7 @@ var map;
 
             function initMap() {
                 map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
+                    zoom: 4.5,
                     center: centerCords
                 });
                 addMarkerInfo();
